5a4f3d54334977e63a7ca8245e5e182bbda602d3
Kristina <krdiamond@gmail.com>
2019-02-18 16:13:41 -0500

    begining to move styles to <style> tag

--- challenge-2/jquery-copy-to-console.js
+++ challenge-2/jquery-copy-to-console.js
@@ -7,21 +7,32 @@ $(document).on('click','.size .selectable',function () {
   $('body').css({'overflow': 'hidden' });
   $('body').prepend(`
       <!-- black curtain over site -->
-      <div class="curtain" style="background: black; opacity: .75; width: 100vw; position: fixed; height: 100vh; z-index: 99;"></div>
+      <div class="curtain"></div>
       <!-- modal overlay -->
-      <div class="overlay" style="background:white;position: fixed; left: 0; right: 0; bottom: 0; top: 0; margin: 20%; z-index: 100;">
+      <div class="overlay" style="background:white; position:fixed; z-index:100;top:50%; left:50%; transform:translate(-50%,-50%);">
         <div class="close" style="cursor: pointer; border: 3px solid;position: absolute;right: 0;top: 0;width: 30px;height: 30px;font-size: 24px;text-align: center;line-height: 1;">X</div>
-        <div class='image-wrapper' style="""></div
-          <div class='right-wrapper'>
-            <div class="text-wrapper" style="font-size: 36px;text-align: right;font-family: 'ars_maquette_probold';">Get this <span class='overlay-product-name'></span> for <span class='overlay-discount-price'></span> with a 15% discount
+        <div class="image-wrapper" style="float:right; width:300px;"></div>
+        <div class="right-wrapper">
+          <div class="text-wrapper" style="font-size:36px; text-align:right; font-family:'ars_maquette_probold'">
+            Get this <span class='overlay-product-name'></span> for <span class='overlay-discount-price'></span> with a 15% Discount
           </div>
         </div>
-      </div>`
+      </div>
+      <style>
+      .curtain {
+        background: black;
+        opacity: .75;
+        width: 100vw;
+        position: fixed;
+        height: 100vh;
+        z-index: 99;
+      }
+      </style>`
     );
     $('.overlay-product-name').text(productName);
     $('.overlay-discount-price').text(discountPrice);
     $('.overlay-discount-price').text('$' + discountPrice);
-    // $('.image-wrapper').append($('.primary-image').first());
+    $('.image-wrapper').append($('.primary-image').first());
     $('.right-wrapper').append('<a href="https://www.marmot.com/cart" style="border: 3px solid black;padding: 12px 36px;box-shadow: 5px 10px black;text-decoration: none; color: black;">CHECKOUT</a>')
     $('.close').click(function(){
         $('.curtain').hide();
